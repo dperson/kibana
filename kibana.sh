59729a62ef52bfad7914d9d3f52dfd16415d84d1
David Personette <dperson@gmail.com>
2015-10-30 05:35:41 -0400

    kibana.sh derp fix changing GID

--- kibana.sh
+++ kibana.sh
@@ -65,7 +65,7 @@ shift $(( OPTIND - 1 ))
 
 [[ "${TZ:-""}" ]] && timezone "$TZ"
 [[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID kibana
-[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && usermod -g $GROUPID kibana
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID kibana
 
 chown -Rh kibana. /opt/kibana 2>&1 | grep -iv 'Read-only' || :
 
